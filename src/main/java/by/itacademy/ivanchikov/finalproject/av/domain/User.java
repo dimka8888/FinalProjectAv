1f0892d0aed63a9bf24008b235aef16f63193dcb
Dmitry-Ivanchikov <dmitry.ivanchikov@gmail.com>
2024-01-27 16:03:55 +0300

    add random Faker correct GPT

--- src/main/java/by/itacademy/ivanchikov/finalproject/av/domain/User.java
+++ src/main/java/by/itacademy/ivanchikov/finalproject/av/domain/User.java
@@ -1,28 +1,30 @@
 package by.itacademy.ivanchikov.finalproject.av.domain;
 import com.github.javafaker.Faker;
 
-public class User {
-    private static Faker faker;
 
-    public static String getRandomNoCorrectEmail() {
-        return faker.internet().password() + "@test.com";
-    }
+    public class User {
+        private static Faker faker = new Faker();
 
-    public static String getRandomCorrectEmail() {
-        return faker.internet().safeEmailAddress(faker.name().firstName()+"@test.com");
-    }
-
-    public static String getRandomPassword() {
-        return faker.internet().password();
-    }
-
-    public static String getRandomPhoneNumber() {
-        StringBuilder phoneNumber = new StringBuilder();
-        phoneNumber.append("29");
-
-        for (int i = 1; i < 7; i++) {
-            phoneNumber.append(faker.number().randomDigit());
+        public static String getRandomNoCorrectEmail() {
+            return faker.internet().password() + "@test.com";
         }
-        return phoneNumber.toString();
+
+        public static String getRandomCorrectEmail() {
+            return faker.name().firstName() + "@" + faker.internet().safeEmailAddress();
+        }
+
+        public static String getRandomPassword() {
+            return faker.internet().password();
+        }
+
+        public static String getRandomPhoneNumber() {
+            StringBuilder phoneNumber = new StringBuilder();
+            phoneNumber.append("29");
+
+            for (int i = 0; i < 7; i++) {
+                phoneNumber.append(faker.number().randomDigit());
+            }
+            return phoneNumber.toString();
+        }
     }
-}
+
